5c730305b6862af0fc277aec5da38800db20e456
iqra_malik31 <iqra93796@gmail.com>
2023-08-06 18:25:13 +0300

    starting

---
+++ src/App.js
@@ -0,0 +1,10 @@
+const App = () =>{
+  return (
+  <>
+  <h1>HELLO</h1>;
+  <span>Sir Raja</span>;
+  <img src="c:\Users\Iqra\Camera\IMG_20170902_020903.jpg" alt=''/>;
+  </>
+  );
+};
+ export default App;
